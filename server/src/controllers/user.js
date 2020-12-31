574d764c5a9b35b0c28ba95eb4adda7edae87de8
mahamud babu <mahmudbabu94@gmail.com>
2020-12-30 21:08:41 -0500

    STARTOVER

--- server/src/controllers/user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-// top text?
-router.get('/getUser', (req, res) => { 
-	// DONT USE THIS IT MAKES NO SENSE IT WAS JUST FOR ROUTE TESTING AGAIN
-	res.status(200).json({msg: "bababooey"});
-	});
-	
-	
-module.exports = router;
