4ad3e107e5418fe2c3247b105ea488c358a1fa60
Mathew Soto <matthewjs1234@aol.com>
2021-01-04 22:51:29 -0500

    connect finnhub api to graphing file

--- StonkStashBackEnd/main.py
+++ StonkStashBackEnd/main.py
@@ -1,13 +1,13 @@
-#this is to avoid a "module not found" error
-#uncomment if you need it
-
-#import sys
-#the path below is the location of most of the python libraries on my device
-#it will likely work on any other mac but that might not be the case
-#sys.path.insert(0, "/usr/local/lib/python3.9/site-packages")
-
 import finnhub
+import time
+import datetime as dt
 
+#week is 7 days
+#month is 30 days
+#quarter is 91 days
+#semiannual is 182 days
+#year is 365 days
+periods = {"w": 604800, "m": 2592000, "q": 2592000, "s": 15724800, "y": 31536000}
 # Setup client
 finnhub_client = finnhub.Client(api_key= "bvml27748v6trsjv9u80")
 
@@ -15,8 +15,13 @@ def getStockPrice(companySymbol):
     stockPriceJSON = finnhub_client.quote(companySymbol)
     highPrice = stockPriceJSON['h']
     lowPrice = stockPriceJSON['l']
-    print(highPrice)
-    print(lowPrice)
+    return highPrice, lowPrice
+
+def getStockCandles(companySymbol,resolution,end,period):
+    end = int(time.mktime(dt.datetime.strptime(end, "%Y-%m-%d").timetuple()))
+    start = end - periods[period]
+    candles = finnhub_client.stock_candles(companySymbol,resolution,start,end)
+    return candles
 
 #to see the high and low price of a stock with symbol AMZN (for example), use the following line
 #getStockPrice('AMZN')
