61e2eec2ac34ec41e1bbfbbba4da135a20c7edda
Nannaquin <stvnmsantana@gmail.com>
2021-01-27 16:03:53 -0500

    Establish initial pages, include forgotton files

--- stonks_frontend/src/App.js
+++ stonks_frontend/src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Landing from './pages/Landing';
 
 export default class App extends Component {
   render() { ///This renders everything
     return (//This will be where you wanna display stuff, in between the divs
-      <div>
-        <h1>HELLO WORLD</h1>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing}/>
+        </Switch>
+      </Router>
     )
   }
 }
