ec45004d6545fb1ed3260acbdb5be5e9e1e8fe8b
Nannaquin <stvnmsantana@gmail.com>
2021-01-27 19:35:59 -0500

    Create Dashboard, add documentation

---
+++ stonks_frontend/src/pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+/** The homepage past login, services available */
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    // state stuff
+  }
+
+  render() {
+    return(<h1>Dashboard: in development</h1>)
+
+  }
+}
+
+export default Dashboard;
