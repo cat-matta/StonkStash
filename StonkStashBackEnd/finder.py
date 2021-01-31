05384812779496015ef37b5e76d0dcf94a3c4462
Mathew Soto <matthewjs1234@aol.com>
2021-01-31 15:48:20 -0500

    Computed all ratios except for those requiring info not on balance sheet

--- StonkStashBackEnd/finder.py
+++ StonkStashBackEnd/finder.py
@@ -4,7 +4,7 @@ import urllib.request
 import bs4 as bs
 import re
 
-stockSymbol = 'gme'
+stockSymbol = 'aapl'
 datastuff = 'https://www.marketwatch.com/investing/stock/' + stockSymbol+ '/financials/balance-sheet/quarter' ##basically the link, we need to find a way to inject the stock name into the link
 
 
@@ -39,6 +39,11 @@ for i in range(endAssets, len(balancelist)):
 #startAssets is the index where the first of the five quarters are in the Assets table in balancelist
 #startLiabilites is the index where the first of the five quarters are in the Liabilities table in balancelist
 
+dates = []
+for i in range(5):
+    dates.append(balancelist[startAssets+i])
+#dates is a list containing the dates the information in assets and liabilities corresponds to
+
 assets = {}
 counter1 = 0
 counter2 = -1
@@ -46,7 +51,7 @@ for i in range(startAssets+6, endAssets):
 
     if counter1 == 0:
         heading = balancelist[i]
-        assets[heading] = {}
+        assets[heading] = []
 
     if counter1 == 2:
         counter2 = 0
@@ -55,7 +60,7 @@ for i in range(startAssets+6, endAssets):
         counter2 = -1
 
     if counter2 != -1:
-        assets[heading][balancelist[startAssets+counter2]] = balancelist[i]
+        assets[heading].append(balancelist[i])
         counter2 = counter2 + 1
 
     counter1 = counter1 + 1
@@ -71,7 +76,7 @@ for i in range(startLiabilities+6, endLiabilities):
 
     if counter1 == 0:
         heading = balancelist[i]
-        liabilities[heading] = {}
+        liabilities[heading] = []
 
     if counter1 == 2:
         counter2 = 0
@@ -80,7 +85,7 @@ for i in range(startLiabilities+6, endLiabilities):
         counter2 = -1
 
     if counter2 != -1:
-        liabilities[heading][balancelist[startLiabilities+counter2]] = balancelist[i]
+        liabilities[heading].append(balancelist[i])
         counter2 = counter2 + 1
 
     counter1 = counter1 + 1
@@ -88,3 +93,37 @@ for i in range(startLiabilities+6, endLiabilities):
         counter1 = 0
 
 #liabilities is a dictionary which has all of the information in the Liabilities table
+
+def turnFloat(value):
+    multiplier = 1
+    if value[-1] == 'M':
+        multiplier = 1000000
+    if value[-1] == 'B':
+        multiplier = 1000000000
+    value = value[:-1]
+    value = float(value)
+    return value*multiplier
+
+currentAssets = turnFloat(assets["Total Current Assets"][4])
+currentLiabilities = turnFloat(liabilities["Total Current Liabilities"][4])
+workingCapital = currentAssets - currentLiabilities
+cwcRatio = currentAssets/currentLiabilities
+inventory = turnFloat(assets["Inventories"][4])
+quickRatio = (currentAssets - inventory)/currentLiabilities
+totalAssets = turnFloat(assets["Total Assets"][4])
+wcOverAssets = workingCapital/totalAssets
+totalLiabilities = turnFloat(liabilities["Total Liabilities"][4])
+netWorth = totalAssets - totalLiabilities
+debtWorthRatio = totalLiabilities/netWorth
+
+print("Current Assets: " + str(currentAssets))
+print("Current Liabilities: " + str(currentLiabilities))
+print("Working Capital: " + str(workingCapital))
+print("Current Working Capital Ratio: " + str(cwcRatio))
+print("Inventory: " + str(inventory))
+print("Quick Ratio: " + str(quickRatio))
+print("Total Assets: " + str(totalAssets))
+print("Working Capital Over Assets: " + str(wcOverAssets))
+print("Total Liabilities: " + str(totalLiabilities))
+print("Net Worth: " + str(netWorth))
+print("Debt Worth Ratio: " + str(debtWorthRatio))
