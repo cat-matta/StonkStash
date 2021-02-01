b650bc642a3622525cf59cbda14433b32545fde3
Mathew Soto <matthewjs1234@aol.com>
2021-02-01 17:46:47 -0500

    Obtain all ratios

--- StonkStashBackEnd/finder.py
+++ StonkStashBackEnd/finder.py
@@ -4,97 +4,68 @@ import urllib.request
 import bs4 as bs
 import re
 
-stockSymbol = 'aapl'
-datastuff = 'https://www.marketwatch.com/investing/stock/' + stockSymbol+ '/financials/balance-sheet/quarter' ##basically the link, we need to find a way to inject the stock name into the link
+#EVERYTHING HAPPENS IN THE driver() FUNCTION AT THE BOTTOM
 
+def getDivListHelper(url):
+    opener = urllib.request.urlopen(url).read() ##opens the html page
+    stonksoup = bs.BeautifulSoup(opener,'lxml')
+    divList = [] ##create a list to add anything with the div tag
+    for stocklabel in stonksoup.find_all('div'):
+        if stocklabel.string:
+            divList.append(stocklabel.string)
 
-opener = urllib.request.urlopen(datastuff).read() ##opens the html page
-stonksoup = bs.BeautifulSoup(opener,'lxml') ##turns it to xml format
+    return divList
 
-verifyDate = re.compile("(\d{2})-([a-zA-Z]{3})-(\d{4})")
+def getDivList(stockSymbol):
+    balanceLink = 'https://www.marketwatch.com/investing/stock/' + stockSymbol + '/financials/balance-sheet/quarter'
+    cashLink = 'https://www.marketwatch.com/investing/stock/' + stockSymbol + '/financials/cash-flow/quarter'
+    balanceList = getDivListHelper(balanceLink)
+    cashList = getDivListHelper(cashLink)
+    return balanceList, cashList
 
-flag = 0
-balancelist = [] ##create a list to add anything with the div tag
-for stocklabel in stonksoup.find_all('div'):
-    balancelist.append(stocklabel.string)
+def getIndices(divList, finalRow, verifyDate):
+    for i in range(len(divList)):
+        if divList[i] == "Item":
+            if verifyDate.fullmatch(divList[i+1]):
+                start = i+1
 
-for i in range(len(balancelist)):
-    if balancelist[i] == "Item":
-        if verifyDate.fullmatch(balancelist[i+1]):
-            startAssets = i+1
+        if divList[i] == finalRow:
+            end = i+7
+            break
 
-    if balancelist[i] == "Total Assets Growth":
-        endAssets = i+7
-        break
+    return start, end
 
-for i in range(endAssets, len(balancelist)):
-    if balancelist[i] == "Item":
-        if verifyDate.fullmatch(balancelist[i+1]):
-            startLiabilities = i+1
+def infoToDict(divList, start, end):
+    diction = {}
+    counter1 = 0
+    counter2 = -1
+    for i in range(start+6, end):
 
-    if balancelist[i] == "Liabilities & Shareholders' Equity":
-        endLiabilities = i+7
-        break
+        if counter1 == 0:
+            heading = divList[i]
+            diction[heading] = []
 
-#startAssets is the index where the first of the five quarters are in the Assets table in balancelist
-#startLiabilites is the index where the first of the five quarters are in the Liabilities table in balancelist
+        if counter1 == 2:
+            counter2 = 0
 
-dates = []
-for i in range(5):
-    dates.append(balancelist[startAssets+i])
-#dates is a list containing the dates the information in assets and liabilities corresponds to
+        if counter2 == 5:
+            counter2 = -1
 
-assets = {}
-counter1 = 0
-counter2 = -1
-for i in range(startAssets+6, endAssets):
+        if counter2 != -1:
+            diction[heading].append(divList[i])
+            counter2 = counter2 + 1
 
-    if counter1 == 0:
-        heading = balancelist[i]
-        assets[heading] = []
+        counter1 = counter1 + 1
+        if counter1 == 7:
+            counter1 = 0
 
-    if counter1 == 2:
-        counter2 = 0
-
-    if counter2 == 5:
-        counter2 = -1
-
-    if counter2 != -1:
-        assets[heading].append(balancelist[i])
-        counter2 = counter2 + 1
-
-    counter1 = counter1 + 1
-    if counter1 == 10:
-        counter1 = 0
-
-#assets is a dictionary which has all of the information in the Assets table
-
-liabilities = {}
-counter1 = 0
-counter2 = -1
-for i in range(startLiabilities+6, endLiabilities):
-
-    if counter1 == 0:
-        heading = balancelist[i]
-        liabilities[heading] = []
-
-    if counter1 == 2:
-        counter2 = 0
-
-    if counter2 == 5:
-        counter2 = -1
-
-    if counter2 != -1:
-        liabilities[heading].append(balancelist[i])
-        counter2 = counter2 + 1
-
-    counter1 = counter1 + 1
-    if counter1 == 10:
-        counter1 = 0
-
-#liabilities is a dictionary which has all of the information in the Liabilities table
+    return diction
 
 def turnFloat(value):
+    parity = 1
+    if value[0] == '(':
+        value = value[1:-1]
+        parity = -1
     multiplier = 1
     if value[-1] == 'M':
         multiplier = 1000000
@@ -102,28 +73,74 @@ def turnFloat(value):
         multiplier = 1000000000
     value = value[:-1]
     value = float(value)
-    return value*multiplier
+    return value*multiplier*parity
 
-currentAssets = turnFloat(assets["Total Current Assets"][4])
-currentLiabilities = turnFloat(liabilities["Total Current Liabilities"][4])
-workingCapital = currentAssets - currentLiabilities
-cwcRatio = currentAssets/currentLiabilities
-inventory = turnFloat(assets["Inventories"][4])
-quickRatio = (currentAssets - inventory)/currentLiabilities
-totalAssets = turnFloat(assets["Total Assets"][4])
-wcOverAssets = workingCapital/totalAssets
-totalLiabilities = turnFloat(liabilities["Total Liabilities"][4])
-netWorth = totalAssets - totalLiabilities
-debtWorthRatio = totalLiabilities/netWorth
+def driver():
+    #put in the stock symbol you want
+    stockSymbol = 'aapl'
 
-print("Current Assets: " + str(currentAssets))
-print("Current Liabilities: " + str(currentLiabilities))
-print("Working Capital: " + str(workingCapital))
-print("Current Working Capital Ratio: " + str(cwcRatio))
-print("Inventory: " + str(inventory))
-print("Quick Ratio: " + str(quickRatio))
-print("Total Assets: " + str(totalAssets))
-print("Working Capital Over Assets: " + str(wcOverAssets))
-print("Total Liabilities: " + str(totalLiabilities))
-print("Net Worth: " + str(netWorth))
-print("Debt Worth Ratio: " + str(debtWorthRatio))
+    #lists containing all of the strings in div tags
+    balanceList, cashFlowList = getDivList(stockSymbol)
+
+    #regex saying that something is in the form of a date
+    verifyDate = re.compile("(\d{2})-([a-zA-Z]{3})-(\d{4})")
+
+    #indices of the lists which sandwich the info we need
+    startAssets, endAssets = getIndices(balanceList, "Total Assets Growth", verifyDate)
+    startLiabilities, endLiabilities = getIndices(balanceList, "Liabilities & Shareholders' Equity", verifyDate)
+    startOperatingActivities, endOperatingActivities = getIndices(cashFlowList, "Net Operating Cash Flow / Sales", verifyDate)
+    startInvestingActivities, endInvestingActivities = getIndices(cashFlowList, "Net Investing Cash Flow / Sales", verifyDate)
+    startFinancingActivities, endFinancingActivities = getIndices(cashFlowList, "Free Cash Flow Yield", verifyDate)
+
+    #dictionaries containing the info we need in a nice format
+    assets = infoToDict(balanceList, startAssets, endAssets)
+    liabilities = infoToDict(balanceList, startLiabilities, endLiabilities)
+    operating = infoToDict(cashFlowList, startOperatingActivities, endOperatingActivities)
+    investing = infoToDict(cashFlowList, startInvestingActivities, endInvestingActivities)
+    financing = infoToDict(cashFlowList, startFinancingActivities, endFinancingActivities)
+
+    #all of the info asked for
+    currentAssets = turnFloat(assets["Total Current Assets"][4])
+    currentLiabilities = turnFloat(liabilities["Total Current Liabilities"][4])
+    workingCapital = currentAssets - currentLiabilities
+    cwcRatio = currentAssets/currentLiabilities
+    inventory = turnFloat(assets["Inventories"][4])
+    quickRatio = (currentAssets - inventory)/currentLiabilities
+    totalAssets = turnFloat(assets["Total Assets"][4])
+    wcOverAssets = workingCapital/totalAssets
+    totalLiabilities = turnFloat(liabilities["Total Liabilities"][4])
+    netWorth = totalAssets - totalLiabilities
+    debtWorthRatio = totalLiabilities/netWorth
+    cash = turnFloat(assets["Cash Only"][4])
+    cANDst = turnFloat(assets["Cash & Short Term Investments"][4])
+    freeCashFlow = turnFloat(financing["Free Cash Flow"][4])
+    netIncome = turnFloat(operating["Net Income before Extraordinaries"][4])
+    flowToIncome = freeCashFlow/netIncome
+    tmoc = workingCapital/4
+    cashOverTMOC = cash/tmoc
+    castOverTMOC = cANDst/tmoc
+
+    #uncomment below to see the values
+    '''
+    print("Current Assets: " + str(currentAssets))
+    print("Current Liabilities: " + str(currentLiabilities))
+    print("Working Capital: " + str(workingCapital))
+    print("Current Working Capital Ratio: " + str(cwcRatio))
+    print("Inventory: " + str(inventory))
+    print("Quick Ratio: " + str(quickRatio))
+    print("Total Assets: " + str(totalAssets))
+    print("Working Capital Over Assets: " + str(wcOverAssets))
+    print("Total Liabilities: " + str(totalLiabilities))
+    print("Net Worth: " + str(netWorth))
+    print("Debt Worth Ratio: " + str(debtWorthRatio))
+    print("Cash: " + str(cash))
+    print("Cash and Short Term Investments: " + str(cANDst))
+    print("Free Cash Flow: " + str(freeCashFlow))
+    print("Net Income: " + str(netIncome))
+    print("Flow to Income: " + str(flowToIncome))
+    print("3 Month Operating Costs: " + str(tmoc))
+    print("Cash over 3 Month Operating Costs: " + str(cashOverTMOC))
+    print("Cash and Short Term Investments over 3 Month Operating Costs: " + str(castOverTMOC))
+    '''
+
+driver()
