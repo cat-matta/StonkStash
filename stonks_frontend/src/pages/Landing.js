3577b6019053ddc1e8c11fe6b3d75f0f86dd6314
Nannaquin <stvnmsantana@gmail.com>
2021-02-13 11:34:53 -0500

    Flesh out bottom of Landing

--- stonks_frontend/src/pages/Landing.js
+++ stonks_frontend/src/pages/Landing.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
-import {Button, Container, Col, Row} from 'react-bootstrap';
+import {Container, Col, Row, Button, Form, Image} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 import '../css/landing.css';
 
 import LandingNavbar from '../components/LandingNavbar';
 import LandingSplash from '../components/LandingSplash';
 import LandingVertCarousel from '../components/LandingVertCarousel';
+
+import bottom_stache from '../images/bottom_stache.png';
 
 require('bootstrap');
 
@@ -29,6 +32,30 @@ Investing in the stock market is not hard; like most things, it entails a learni
 learning curve and serve as your educational tool to understanding the principles and mindset of successful investors.</p>
         </Row>
         <LandingVertCarousel/>
+        <Row id="closing_rect">
+          <div id="closing_header" className="light">When You Make it Over the Learning Curve: Delete Us!</div>
+          <p id="closing_text" className="light">Or hold on to this tool for its no-nonsense display of volatility and balance sheets and to reinforce the basic but nonetheless essential pillars to successful long-term investing.</p>
+          <p id="closing_disclaimer">Stonkstache is for information and educational purposes only. It is not intended to be an investment advice. Please consider the risk involved and your personal financial situation before investing or seek a duly licensed professional for investment advice.</p>
+        </Row>
+        <Row id="footer_rect">
+          <Col sm={3}>
+            <Link id="terms_link" className="light" to="#terms">Terms & Conditions</Link>
+            <Link id="disclaimers_link" className="light" to="#disclaimers">Disclaimers</Link>
+            <Link id="privacy_link" className="light" to="#privacy">Privacy</Link>
+          </Col>
+          <Col sm={9}>
+            
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label id="newsletter_text" className="light">Join our monthly newsletter for early access to any information regarding the launch!</Form.Label>
+              <Form.Control id="newsletter_input_field" className="light" type="email" placeholder="Email..."/>
+              <Button id="newsletter_submit_button" className="light" variant="">Join!</Button>
+            </Form.Group>
+         </Col>
+        </Row>
+        <Row>
+          <Image id="bottom_stache" src={bottom_stache}/>
+          <div id="all_rights_text" className="dark">2021 Stonkstache. All Rights Reserved.</div>
+        </Row>
       </Container>
     )
   }
